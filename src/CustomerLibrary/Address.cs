3bd32d8758c699c25336513460d9f23fe4130989
Alexander Slesarenko <dragongling@gmail.com>
2021-11-11 21:31:12 +0500

    feat: Added `Address`

---
+++ src/CustomerLibrary/Address.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerLibrary
+{
+    public enum Type
+    {
+        Shipping,
+        Billing
+    }
+
+    public class Address
+    {
+        public string Line { get; }
+
+        public string? Line2 { get; }
+
+        public Type Type { get; }
+
+        public string City { get; }
+
+        public string PostalCode { get; }
+
+        public string State { get; }
+
+        public string Country { get; }
+
+        public Address(
+            string line, 
+            Type type, 
+            string city, 
+            string postalCode, 
+            string state, 
+            string country
+            )
+        {
+            Line = line;
+            Type = type;
+            City = city;
+            PostalCode = postalCode;
+            State = state;
+            Country = country;
+        }
+
+        public Address(
+            string line, 
+            string line2, 
+            Type type, 
+            string city, 
+            string postalCode, 
+            string state, 
+            string country
+            )
+        {
+            Line = line;
+            Line2 = line2;
+            Type = type;
+            City = city;
+            PostalCode = postalCode;
+            State = state;
+            Country = country;
+        }        
+    }
+}
---
+++ tests/CustomerLibrary.Tests/AddressTests.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+
+namespace CustomerLibrary.Tests
+{
+    public class AddressTests
+    {
+        [Fact]
+        public void ShouldCreateAddress()
+        {
+            const string line = "Line";
+            const string line2 = "Line2";
+            const string city = "City";
+            const string postalCode = "123456";
+            const string state = "State";
+            const string country = "Country";
+
+            // Required fields only
+            var address = new Address(
+                line,
+                Type.Shipping,
+                city,
+                postalCode,
+                state,
+                country
+                );
+
+            Assert.Equal(line, address.Line);
+            Assert.Equal(Type.Shipping, address.Type);
+            Assert.Equal(city, address.City);
+            Assert.Equal(postalCode, address.PostalCode);
+            Assert.Equal(state, address.State);
+            Assert.Equal(country, address.Country);
+
+            // All fields
+            var address2 = new Address(
+                line,
+                line2,
+                Type.Shipping,
+                city,
+                postalCode,
+                state,
+                country
+                );
+
+            Assert.Equal(line, address2.Line);
+            Assert.Equal(line2, address2.Line2);
+            Assert.Equal(Type.Shipping, address2.Type);
+            Assert.Equal(city, address2.City);
+            Assert.Equal(postalCode, address2.PostalCode);
+            Assert.Equal(state, address2.State);
+            Assert.Equal(country, address2.Country);
+        }
+    }
+}
