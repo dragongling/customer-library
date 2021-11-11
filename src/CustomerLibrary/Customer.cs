9870b226b557efc062e2999abfc83349e80cba94
Alexander Slesarenko <dragongling@gmail.com>
2021-11-11 22:25:08 +0500

    feat: Added `Customer`

---
+++ src/CustomerLibrary/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerLibrary
+{
+    public class Customer : Person
+    {
+        public List<Address> Addresses { get; }
+        public List<string> Notes { get; }
+        public string? Phone { get; }
+        public string? Email { get; }
+        public decimal? TotalPurchasesAmount { get; }
+
+        public Customer(
+            string lastName, 
+            string? firstName, 
+            ICollection<Address> addresses, 
+            ICollection<string> notes, 
+            string? phone = null, 
+            string? email = null,
+            decimal? totalPurchasesAmount = null
+            ) : base(lastName, firstName)
+        {
+            Addresses = addresses.ToList();
+            Phone = phone;
+            Email = email;
+            Notes = notes.ToList();
+            TotalPurchasesAmount = totalPurchasesAmount;
+        }
+
+        public Customer(
+            string lastName,
+            ICollection<Address> addresses,
+            ICollection<string> notes,
+            string? phone = null,
+            string? email = null,
+            decimal? totalPurchasesAmount = null
+            ) : this(lastName, null, addresses, notes, phone, email, totalPurchasesAmount)
+        {
+        }
+    }
+}
---
+++ tests/CustomerLibrary.Tests/CustomerTests.cs
@@ -0,0 +1,69 @@
+﻿using Xunit;
+
+namespace CustomerLibrary.Tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void ShouldCreateCustomer()
+        {
+
+            var addresses = new Address[]{ 
+                new Address(
+                    "Line",
+                    Type.Shipping,
+                    "City",
+                    "123456",
+                    "State",
+                    "Canada"
+                ),
+                new Address(
+                    "Line2",
+                    Type.Billing,
+                    "City2",
+                    "123457",
+                    "State2",
+                    "United States"
+                )
+            };
+
+            const string lastName = "LastName";
+            string[] notes = new string[] { "Note", "Note2" };
+
+            // Required fields only
+            var customer = new Customer(
+                lastName,
+                addresses,
+                notes
+            );
+
+            Assert.Equal(lastName, customer.LastName);
+            Assert.Equal(addresses, customer.Addresses);
+            Assert.Equal(notes, customer.Notes);
+
+            const string firstName = "FirstName";
+            const string phone = "8(800)555-35-35";
+            const string email = "mail@example.com";
+            const decimal totalPurchasesAmount = 12.34M;
+
+            // All fields
+            customer = new Customer(
+                lastName,
+                firstName,
+                addresses,
+                notes,
+                phone,
+                email,                
+                totalPurchasesAmount
+                );
+
+            Assert.Equal(lastName, customer.LastName);
+            Assert.Equal(firstName, customer.FirstName);
+            Assert.Equal(addresses, customer.Addresses);
+            Assert.Equal(notes, customer.Notes);
+            Assert.Equal(phone, customer.Phone);
+            Assert.Equal(email, customer.Email);
+            Assert.Equal(totalPurchasesAmount, customer.TotalPurchasesAmount);
+        }
+    }
+}
